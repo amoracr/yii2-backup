7f1939ba5c98b0852cdf09b507219ee32f207d0d
Alonso Mora <alonso.mora@gmail.com>
2020-07-02 01:01:47 -0600

    Refactor inheritance of tar based files

--- src/archive/Gzip.php
+++ src/archive/Gzip.php
@@ -9,16 +9,14 @@ use \PharData;
 use \BadMethodCallException;
 use \Exception;
 use \UnexpectedValueException;
-use \RecursiveIteratorIterator;
-use \RecursiveDirectoryIterator;
-use amoracr\backup\archive\Archive;
+use amoracr\backup\archive\Tar as TarArchive;
 
 /**
  * Description of Gzip
  *
  * @author alonso
  */
-class Gzip extends Archive
+class Gzip extends TarArchive
 {
 
     public function init()
@@ -35,55 +33,6 @@ class Gzip extends Archive
         if (!Phar::canCompress(Phar::GZ)) {
             throw new InvalidConfigException('Extension "zlib" must be enabled.');
         }
-    }
-
-    public function addFileToBackup($name, $file)
-    {
-        try {
-            $archiveFile = new PharData($this->backup);
-            $relativePath = $name . DIRECTORY_SEPARATOR;
-            $relativePath .= pathinfo($file, PATHINFO_BASENAME);
-            $archiveFile->addFile($file, $relativePath);
-        } catch (UnexpectedValueException $ex) {
-            Yii::error("Could not open '{$this->backup}'. Details: " . $ex->getMessage());
-            return false;
-        } catch (BadMethodCallException $ex) {
-            Yii::error("Technically, this should not happen. Details: " . $ex->getMessage());
-            return false;
-        } catch (Exception $ex) {
-            Yii::error("Unable to use backup file. Details: " . $ex->getMessage());
-            return false;
-        }
-        return true;
-    }
-
-    public function addFolderToBackup($name, $folder)
-    {
-        try {
-            $archiveFile = new PharData($this->backup);
-            $files = new RecursiveIteratorIterator(
-                    new RecursiveDirectoryIterator(Yii::getAlias($folder)), RecursiveIteratorIterator::LEAVES_ONLY
-            );
-
-            foreach ($files as $file) {
-                $fileName = $file->getFilename();
-                if (!$file->isDir() && !in_array($fileName, $this->skipFiles)) {
-                    $filePath = $file->getRealPath();
-                    $relativePath = $name . DIRECTORY_SEPARATOR . substr($filePath, strlen(Yii::getAlias($folder)) + 1);
-                    $archiveFile->addFile($filePath, $relativePath);
-                }
-            }
-        } catch (UnexpectedValueException $ex) {
-            Yii::error("Could not open '{$this->backup}'. Details: " . $ex->getMessage());
-            return false;
-        } catch (BadMethodCallException $ex) {
-            Yii::error("Technically, this should not happen. Details: " . $ex->getMessage());
-            return false;
-        } catch (Exception $ex) {
-            Yii::error("Unable to use backup file. Details: " . $ex->getMessage());
-            return false;
-        }
-        return true;
     }
 
     public function close()
@@ -105,22 +54,6 @@ class Gzip extends Archive
             Yii::error("Unable to use backup file. Details: " . $ex->getMessage());
             return false;
         }
-    }
-
-    public function extractFileFromBackup($name, $file)
-    {
-        try {
-            $archiveFile = new PharData($this->backup);
-            $content = $archiveFile[$name]->getContent();
-            file_put_contents($file, $content);
-        } catch (UnexpectedValueException $ex) {
-            Yii::error("Could not open '{$this->backup}'. Details: " . $ex->getMessage());
-        } catch (BadMethodCallException $ex) {
-            Yii::error("Technically, this should not happen. Details: " . $ex->getMessage());
-        } catch (Exception $ex) {
-            Yii::error("Unable to use backup file. Details: " . $ex->getMessage());
-        }
-        return file_exists($file);
     }
 
 }
