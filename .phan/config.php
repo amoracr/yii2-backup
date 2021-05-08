deeee195d38460edd29fdacd1dcff510c07b1f02
Alonso Mora <alonso.mora@gmail.com>
2021-05-07 19:55:17 -0600

    Add checking for PHP 5.6

--- .phan/config.php
+++ .phan/config.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    "target_php_version" => '7.0,7.1,7.2,7.3,7.4,8.0',
+    "target_php_version" => '5.6,7.0,7.1,7.2,7.3,7.4,8.0',
     'directory_list' => [
         'src',
         'vendor'
