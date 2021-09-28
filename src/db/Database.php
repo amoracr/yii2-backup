38caed94833ca23af190c44da2f43e68b344bdf3
Alonso Mora <alonso.mora@gmail.com>
2021-09-27 21:31:10 -0600

    Refactor base class for database handlers

--- src/db/Database.php
+++ src/db/Database.php
@@ -10,7 +10,6 @@
 namespace amoracr\backup\db;
 
 use yii\base\Component;
-use yii\base\InvalidConfigException;
 
 /**
  * Abstract class for managing the database engines.
@@ -21,11 +20,29 @@ use yii\base\InvalidConfigException;
 abstract class Database extends Component
 {
 
-    /**  @var string Command to use for dumping data */
-    public $dumpCommand;
+    /**  @var string Database name */
+    protected $dbName;
 
-    /**  @var string  Command to use for importing data */
-    public $loadCommand;
+    /**  @var string Database version */
+    protected $dbVersion;
+
+    /**  @var string Database engine name */
+    protected $dbEngine;
+
+    /**  @var string Delimiter for query statements */
+    protected $delimiter;
+
+    /**  @var string Full path of the dump file  */
+    protected $dumpFile;
+
+    /** @var mixed Database connection object */
+    protected $connection;
+
+    /** @var array Comments at the begining of dump file */
+    protected $beginCommands = [];
+
+    /** @var array Comments at the end of dump file */
+    protected $endCommands = [];
 
     /**
      * Dumps database data into a file
@@ -44,59 +61,170 @@ abstract class Database extends Component
     abstract public function importDatabase($dbHandle, $file);
 
     /**
-     * Prepares command according to datase connection
+     * Initializes the database connection
      *
      * @param string $dbHandle Database connection name
-     * @param string $templateCommand Template command to use
      */
-    abstract protected function prepareCommand($dbHandle, $templateCommand);
+    abstract protected function initDb($dbHandle);
 
     /**
-     * Checks if property dumpCommand is valid
+     * Add special quotes to string
      *
-     * @return boolean True if property value is valid
-     * @throws InvalidConfigException if the property value is not valid
+     * @param string $string String to add special quotes for SQL
+     * @return string String with special quotes for SQL
      */
-    protected function validateDumpCommand()
+    abstract protected function escapeName($string);
+
+    /**
+     * Dumps schema of a database table in a file
+     *
+     * @param string $table Name of table
+     */
+    abstract protected function dumpTableSchema($table);
+
+    /**
+     * Dumps data of a database table in a file
+     *
+     * @param string $table Name of table
+     */
+    abstract protected function dumpTableData($table);
+
+    /**
+     * Dumps triggers of a database table in a file
+     *
+     * @param string $tableTrigger Name of table
+     */
+    abstract protected function dumpTrigger($tableTrigger);
+
+    /**
+     * Dumps data of a database view in a file
+     *
+     * @param string $view Name of table
+     */
+    abstract protected function dumpView($view);
+
+    /**
+     * Dumps data of a database function in a file
+     *
+     * @param string $function Name of function
+     */
+    abstract protected function dumpFunction($function);
+
+    /**
+     * Dumps data of a database procedure in a file
+     *
+     * @param string $procedure Name of procedure
+     */
+    abstract protected function dumpProcedure($procedure);
+
+    /**
+     * Dumps data of a database index in a file
+     *
+     * @param string $index Name of index
+     */
+    abstract protected function dumpIndex($index);
+
+    /**
+     * Gets the list of user defined indexes of database
+     *
+     * @return array Array with list of indexes names
+     */
+    abstract protected function getIndexes();
+
+    /**
+     * Gets the list of tables of database
+     *
+     * @return array Array with list of tables names
+     */
+    abstract protected function getTables();
+
+    /**
+     * Gets the list of views of database
+     *
+     * @return array Array with list of views names
+     */
+    abstract protected function getViews();
+
+    /**
+     * Gets the list of triggers of database
+     *
+     * @return array Array with list of triggers names
+     */
+    abstract protected function getTriggers();
+
+    /**
+     * Gets the list of stored functions of database
+     *
+     * @return array Array with list of stored functions names
+     */
+    abstract protected function getFunctions();
+
+    /**
+     * Gets the list of stored procedures of database
+     *
+     * @return array Array with list of stored procedures names
+     */
+    abstract protected function getProcedures();
+
+    /**
+     * Gets the column names of a table
+     *
+     * @param string $table Name of table
+     * @param array $numericColumns Array for storing if a column is numeric or not
+     * @return string String with the columns names of table
+     */
+    abstract protected function getTableColumns(&$table, &$numericColumns);
+
+    /**
+     * Gets the data of a row
+     *
+     * @param array $row Row Array with result set from a query
+     * @param array $numericColumns Array that indicates if a column is numeric or not
+     * @return string String with the values ready to be put in a file
+     */
+    abstract protected function getTableRowValues(&$row, &$numericColumns);
+
+    /**
+     * Adds heading comments to dump file
+     */
+    protected function openDump()
     {
-        if (!is_string($this->dumpCommand)) {
-            throw new InvalidConfigException('"' . get_class($this) . '::dumpCommand" should be string, "' . gettype($this->dumpCommand) . '" given.');
-        } else if (empty($this->dumpCommand)) {
-            throw new InvalidConfigException('"' . get_class($this) . '::dumpCommand" can not be empty"');
+        if (file_exists($this->dumpFile)) {
+            @unlink($this->dumpFile);
         }
-        return true;
+
+        $engine = sprintf("-- %s database dump\n", $this->dbEngine);
+        $version = sprintf("-- %s version: %s\n", $this->dbEngine, $this->dbVersion);
+        $database = sprintf("-- Database: %s\n", $this->dbName);
+        $this->saveToFile("-- ------------------------------------------------------\n");
+        $this->saveToFile($engine);
+        $this->saveToFile($database);
+        $this->saveToFile($version);
+        $this->saveToFile("-- ------------------------------------------------------\n\n");
+
+        foreach ($this->beginCommands as $command) {
+            $this->saveToFile($command);
+        }
     }
 
     /**
-     * Checks if property loadCommand is valid
-     *
-     * @return boolean True if property value is valid
-     * @throws InvalidConfigException if the property value is not valid
+     * Adds closing comments to dump file
      */
-    protected function validateLoadCommand()
+    protected function closeDump()
     {
-        if (!is_string($this->loadCommand)) {
-            throw new InvalidConfigException('"' . get_class($this) . '::loadCommand" should be string, "' . gettype($this->loadCommand) . '" given.');
-        } else if (empty($this->loadCommand)) {
-            throw new InvalidConfigException('"' . get_class($this) . '::loadCommand" can not be empty"');
+        foreach ($this->endCommands as $command) {
+            $this->saveToFile($command);
         }
-        return true;
     }
 
     /**
-     * Replaces parameter names with their respective values
+     * Adds content to dump file
      *
-     * @param string $templateCommand Template command to use
-     * @param array $params List of parameters and its values
-     * @return string Final command to use
+     * @param string $text Content to save
      */
-    protected function replaceParams($templateCommand, $params)
+    protected function saveToFile($text)
     {
-        $command = $templateCommand;
-        foreach ($params as $key => $value) {
-            $command = str_replace('{' . $key . '}', $value, $command);
-        }
-        return $command;
+        file_put_contents($this->dumpFile, $text, FILE_APPEND);
     }
 
 }
